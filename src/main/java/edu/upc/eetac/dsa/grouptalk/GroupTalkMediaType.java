84c228302d1f67f1a32d776c62365eec4fd0fc41
Max <max9@openmailbox.org>
2016-04-12 14:24:46 +0200

    Done

--- src/main/java/edu/upc/eetac/dsa/grouptalk/GroupTalkMediaType.java
+++ src/main/java/edu/upc/eetac/dsa/grouptalk/GroupTalkMediaType.java
@@ -13,16 +13,3 @@ public class GroupTalkMediaType
     public final static String GROUPTALK_GROUP_COLLECTION = "application/vnd.dsa.grouptalk.group.collection+json";
     public final static String GROUPTALK_ROOT = "application/vnd.dsa.grouptalk.root+json";
 }
-
-
-/*
-public class BeeterMediaType
-{
-    public final static String BEETER_AUTH_TOKEN = "application/vnd.dsa.beeter.auth-token+json";
-    public final static String BEETER_USER = "application/vnd.dsa.beeter.user+json";
-    public final static String BEETER_STING = "application/vnd.dsa.beeter.sting+json";
-    public final static String BEETER_STING_COLLECTION = "application/vnd.dsa.beeter.sting.collection+json";
-    public final static String BEETER_ROOT = "application/vnd.dsa.beeter.root+json";
-}
-
- */
